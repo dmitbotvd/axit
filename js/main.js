ed2b861df8b82a729f9ec12d9a065ccb2812a1f7
bot <30392154+dmitbotvd@users.noreply.github.com>
2019-04-22 20:30:09 +0300

    aps

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	$("a[href*=#]").on("click", function(e){
+		var anchor = $(this);
+		$('html, body').stop().animate({
+			scrollTop: $(anchor.attr('href')).offset().top
+		}, 777);
+		e.preventDefault();
+		return false;
+	});
+});
+
+$(document).ready(function(){ 
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').fadeIn();
+        } else {
+            $('.scrollup').fadeOut();
+        }
+});
+
+$('.scrollup').click(function(){
+    $("html, body").animate({ scrollTop: 0 }, 600);
+    return false;
+});
+ 
+});
