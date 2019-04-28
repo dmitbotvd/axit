850b2cd911243f4d558eacfbe7156486adf4f9dc
bot <30392154+dmitbotvd@users.noreply.github.com>
2019-04-28 16:51:57 +0300

    reload

--- js/main.js
+++ js/main.js
@@ -24,4 +24,8 @@ $('.scrollup').click(function(){
 });
  
 });
-
+$('.menu-btn').on('click', function(e){
+    e.preventDefault;
+    $(this).toggleClass('menu-btn_active');
+    $('.menu-nav').toggleClass('menu-nav_active');
+});
